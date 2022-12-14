2a52af6a7ecc1051f89c273da499971d74994611
CHUCHUDACHUCHU <yoon7548@gmail.com>
2022-12-14 21:44:33 +0900

    FIX: error-handler

--- src/middlewares/error-hander.middleware.js
+++ src/middlewares/error-hander.middleware.js
@@ -4,7 +4,6 @@ const errorLogger = (error, request, response, next) => {
 
 const errorHandler = (error, req, res, next) => {
     if (error.name.includes('Sequelize')) {
-        console.log(error);
         res.status(500).json({ errorMessage: 'Internal Server Error' });
     }
     res.status(error.status || 400).json({ errorMessage: error.message });
