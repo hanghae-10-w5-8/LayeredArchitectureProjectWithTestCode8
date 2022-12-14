6da092e6f68e7bd741c794bbee479361890148ce
CHUCHUDACHUCHU <yoon7548@gmail.com>
2022-12-14 22:32:52 +0900

    FEAT: 게시판 구현 완료

--- src/repositories/post.repository.js
+++ src/repositories/post.repository.js
@@ -1,1 +1,61 @@
+const { Users, Posts, Comments, likes } = require('../models');
+const InternalServerError = require('../exceptions/index.exception.js');
+
+class PostRepository {
+    constructor(Posts, Users, Comments, likes) {
+        this.Posts = Posts;
+        this.Users = Users;
+        this.Comments = Comments;
+        this.likes = likes;
+    }
+
+    createPost = async ({ userId, title, content }) => {
+        const createPostData = await Posts.create(userId, title, content);
+        return createPostData;
+    };
+
+    findAllPost = async () => {
+        const data = await Posts.findAll({
+            include: [
+                { model: Users, attributes: ['nickname'] },
+                { model: likes, as: 'likes', attributes: ['likeId'] },
+            ],
+            // 내림차순 정렬
+            order: [['createdAt', 'DESC']],
+        });
+        return data;
+    };
+
+    findPostById = async (postId) => {
+        const post = await Posts.findOne({
+            where: { postId },
+            include: [
+                { model: Users, attributes: ['nickname'] },
+                { model: likes, as: 'likes', attributes: ['likeId'] },
+                {
+                    model: Comments,
+                    as: 'Comments',
+                    order: [['createdAt', 'DESC']],
+                    attributes: [
+                        'commentId',
+                        'content',
+                        'createdAt',
+                        'updatedAt',
+                    ],
+                    include: [{ model: Users, attributes: ['nickname'] }],
+                },
+            ],
+        });
+        return post;
+    };
+
+    updatePost = async ({ postId, title, content }) => {
+        const result = await Posts.update(
+            { title, content },
+            { where: { postId: postId } }
+        );
+        return result;
+    };
+}
+
 module.exports = PostRepository;
--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const PostController = require('../controllers/post.controller.js');
+const postController = new PostController();
+
+router.post('/', postController.createPost);
+router.get('/', postController.getPosts);
+router.get('/:postId', postController.getPostById);
+router.put('/:postId', postController.updatePost);
+
 module.exports = router;
